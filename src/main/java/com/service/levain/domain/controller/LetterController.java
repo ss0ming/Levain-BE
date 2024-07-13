d7ac17df6f84105f67276b872095029e6da5ea50
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-13 23:16:07 +0900

    feat: 페이지 네이션 구현

--- src/main/java/com/service/levain/domain/controller/LetterController.java
+++ src/main/java/com/service/levain/domain/controller/LetterController.java
@@ -1,9 +1,13 @@
 package com.service.levain.domain.controller;
 
+import com.service.levain.domain.dto.letter.request.PageReqDTO;
 import com.service.levain.domain.dto.letter.request.ReqDTO;
 import com.service.levain.domain.entity.Letter;
+import com.service.levain.domain.entity.Member;
 import com.service.levain.domain.service.LetterService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -17,14 +21,12 @@ import java.util.HashMap;
 public class LetterController {
     private final LetterService letterService;
 
-    @PostMapping
-    public ResponseEntity registLetter(@RequestBody HashMap<String, Object>requestJsonMap, Authentication authentication){
-        System.out.println("편지 생성 요청 들어옴");
-
+    @PostMapping("")
+    public ResponseEntity letterRegist(@RequestBody HashMap<String, Object>requestJsonMap, Authentication authentication){
         String content = (String)requestJsonMap.get("content");
         String writer = (String)requestJsonMap.get("writer");
         int iconNum = (Integer)requestJsonMap.get("iconNum");
-        String userName = "kangyuseok"; //임시
+        String userName = "tony_kang"; //임시
 
         ReqDTO reqDTO = new ReqDTO(content, writer, iconNum, userName);
         System.out.println("reqDTO = "+ reqDTO);
@@ -34,10 +36,23 @@ public class LetterController {
         return new ResponseEntity(HttpStatus.OK);
     }
 
-    @GetMapping
-    public ResponseEntity letterList(@RequestBody HashMap<String, Object>requestJsonMap){
-        return null;
+    @GetMapping("")
+    public ResponseEntity letterList(@RequestBody PageReqDTO reqDTO, Authentication authentication){
+        System.out.println("회원 전체 편지 리스트 조회");
+
+        String userName = "tony_kang";
+        Page<Letter> paging = letterService.findAllLetter(reqDTO,userName);
+        for(Letter l : paging) {
+            System.out.println(l);
+            Member m  = l.getMember();
+            m = null;
+        }
+        return new ResponseEntity(paging, HttpStatus.OK);
     }
 
-
+    @GetMapping("/{letterId}")
+    public ResponseEntity viewLetter(@PathVariable("letterId") Long letterId){
+        Letter letter = letterService.findOneLetter(letterId);
+        return new ResponseEntity<>(letter, HttpStatus.OK);
+    }
 }
