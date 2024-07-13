dad9197d88570fface95c2bd064863869bc05afd
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-13 19:16:45 +0900

    fix: builder 패턴으로 테이블 연관관계 생성

--- src/main/java/com/service/levain/domain/entity/Letter.java
+++ src/main/java/com/service/levain/domain/entity/Letter.java
@@ -1,15 +1,14 @@
 package com.service.levain.domain.entity;
 
+import com.service.levain.domain.dto.letter.request.ReqDTO;
 import com.service.levain.domain.enums.DeleteCheck;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import static jakarta.persistence.GenerationType.*;
 
@@ -38,8 +37,17 @@ public class Letter {
     @Enumerated(EnumType.STRING)
     private DeleteCheck isDeleted;
 
-    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @ManyToOne(optional = false)
     @JoinColumn(name = "user_name")
     private Member member;
+
+    // writer, content, iconNum만 포함하는 생성자
+    @Builder
+    public Letter(ReqDTO reqDTO, Member member) {
+        this.writer = reqDTO.getWriter();
+        this.content = reqDTO.getContent();
+        this.iconNum = reqDTO.getIconNum();
+        this.member = member;
+    }
 
 }
