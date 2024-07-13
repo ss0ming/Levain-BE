f83c0183a9ee9948fe31092ef7be74c08089ebfe
강유석 <gang-yuseog@gang-yuseog-ui-MacBookPro.local>
2024-07-13 23:09:55 +0900

    feat: member json 데이터 무시 코드 추가

--- src/main/java/com/service/levain/domain/entity/Member.java
+++ src/main/java/com/service/levain/domain/entity/Member.java
@@ -3,7 +3,6 @@ package com.service.levain.domain.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
