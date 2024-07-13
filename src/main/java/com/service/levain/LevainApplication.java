f1ccb86e6ad1b3882821b837fb1aaf8776c132cc
dongmin132 <m914@naver.com>
2024-07-13 13:13:16 +0900

    initial commit

---
+++ src/main/java/com/service/levain/LevainApplication.java
@@ -0,0 +1,13 @@
+package com.service.levain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LevainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LevainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/levain/LevainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.levain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LevainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
